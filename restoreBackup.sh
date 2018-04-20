07809bb8f9ce74bbf9a2dd188cafe376ea2696dc
againeszdi <38566873+againeszdi@users.noreply.github.com>
2018-04-20 15:17:29 -0500

    Add files via upload

---
+++ restoreBackup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -H "Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz" $1 > /home/zditech/retrieved-backup.tar.gz
+mkdir /home/zditech/retrieved-backup
+tar -zxf /home/zditech/retrieved-backup.tar.gz --directory /home/zditech/retrieved-backup
+cp /home/zditech/retrieved-backup/data/99-usb-serial.rules /data/99-usb-serial.rules 2>/dev/null
+cp /home/zditech/retrieved-backup/data/codec.cfg /data/codec.cfg 2>/dev/null
+cp /home/zditech/retrieved-backup/data/*.json /data/ 2>/dev/null
+if [ -d "/home/zditech/retrieved-backup/data/slides" ]; then
+  mkdir /data/slides 2>/dev/null
+  cp /home/zditech/retrieved-backup/data/slides/*.png /data/slides/ 2>/dev/null
+fi
+rm -r /home/zditech/retrieved-backup
+rm /home/zditech/retrieved-backup.tar.gz
+echo "\n"
+echo "Restore Complete"
+echo "\n"
