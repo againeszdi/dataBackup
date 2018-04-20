88c6cf22aae280141041987726b8a0a238ce5a3d
againeszdi <38566873+againeszdi@users.noreply.github.com>
2018-04-20 17:24:50 -0500

    Update dataBackup.sh

--- dataBackup.sh
+++ dataBackup.sh
@@ -7,7 +7,7 @@ cd /home/zditech
 tar -zcf $1.tar.gz /data
 echo "\n-----------------------------------------\n"
 #send file to spark room
-postData=$(curl -s -X POST -H "Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz" -F "files=@/home/zditech/$1.tar.gz;type=application/gzip" -F "roomId=Y2lzY29zcGFyazovL3VzL1JPT00vZWJmMWE0NTAtMTY1Ni0xMWU3LTg2YzgtMjMzN2U0Nzg0OTlm" -F "text=$1 backup." https://api.ciscospark.com/v1/messages)
+postData=$(curl -X POST -H "Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz" -F "files=@/home/zditech/$1.tar.gz;type=application/gzip" -F "roomId=Y2lzY29zcGFyazovL3VzL1JPT00vZWJmMWE0NTAtMTY1Ni0xMWU3LTg2YzgtMjMzN2U0Nzg0OTlm" -F "text=$1 backup." https://api.ciscospark.com/v1/messages)
 rm /home/zditech/$1.tar.gz
 #send file URL to spark room
 curl -X POST http://recvmsg.cwtest.ultrahook.com -d '$postData'
