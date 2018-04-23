38b95e8a23bb299a526a9fc07409f8614bd1e5b2
againeszdi <38566873+againeszdi@users.noreply.github.com>
2018-04-23 08:15:00 -0500

    Update dataBackup.sh

--- dataBackup.sh
+++ dataBackup.sh
@@ -5,10 +5,13 @@ cd /home/zditech
 
 #create tar file with name specified in command
 tar -zcf $1.tar.gz /data
-echo "\n-----------------------------------------\n"
+echo "-----------------------------------------"
+echo " "
 #send file to spark room
-PostData=$(curl -X POST -H "Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz" -F "files=@/home/zditech/$1.tar.gz;type=application/gzip" -F "roomId=Y2lzY29zcGFyazovL3VzL1JPT00vZWJmMWE0NTAtMTY1Ni0xMWU3LTg2YzgtMjMzN2U0Nzg0OTlm" -F "text=$1 backup." https://api.ciscospark.com/v1/messages)
+postData=$(curl -s -X POST -H "Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz" -F "files=@/home/zditech/$1.tar.gz;type=application/gzip" -F "roomId=Y2lzY29zcGFyazovL3VzL1JPT00vZWJmMWE0NTAtMTY1Ni0xMWU3LTg2YzgtMjMzN2U0Nzg0OTlm" -F "text=$1 backup." https://api.ciscospark.com/v1/messages)
+echo "$postData"
 rm /home/zditech/$1.tar.gz
 #send file URL to spark room
-curl -X POST http://recvmsg.cwtest.ultrahook.com -d "$PostData"
-echo "\n\n-----------------------------------------\nCOPY THE 'files' URL FROM THE ABOVE OUTPUT AND USE IT TO RUN THE restoreBackup.sh SCRIPT ONCE THE GO HAS BEEN MOVED.\n\nExample: sh restoreBackup.sh https://api.ciscospark.com/v1/contents/Y2lzY29zcGFyazovL3VzL0NPTlRFTlQvNzVlNDRjOC0xMWU4LWE5MTYtNzc3OGQ4ODNkNTBiLzA\n"
+curl -s -X POST http://recvmsg.cwtest.ultrahook.com -d "$postData"
+echo "-----------------------------------------"
+echo "COPY THE 'files' URL FROM THE ABOVE OUTPUT AND USE IT TO RUN THE restoreBackup.sh SCRIPT ONCE THE GO HAS BEEN MOVED.\n\nExample: sh restoreBackup.sh https://api.ciscospark.com/v1/contents/Y2lzY29zcGFyazovL3VzL0NPTlRFTlQvNzVlNDRjOC0xMWU4LWE5MTYtNzc3OGQ4ODNkNTBiLzA\n"
