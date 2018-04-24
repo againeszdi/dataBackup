0ebba49314750504e3ee823a823dd73a0b863324
againeszdi <38566873+againeszdi@users.noreply.github.com>
2018-04-24 16:30:45 -0500

    Update restoreBackup.sh

--- restoreBackup.sh
+++ restoreBackup.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
-curl -s -H "Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz" $url > /home/zditech/retrieved-backup.tar.gz
+echo "#!/bin/bash
+read -p \"Restore URL: \" url
+curl -s -H \"Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz\" \$url > /home/zditech/retrieved-backup.tar.gz
 mkdir /home/zditech/retrieved-backup
 tar -zxf /home/zditech/retrieved-backup.tar.gz --directory /home/zditech/retrieved-backup
 cp /home/zditech/retrieved-backup/data/99-usb-serial.rules /data/99-usb-serial.rules 2>/dev/null
 cp /home/zditech/retrieved-backup/data/codec.cfg /data/codec.cfg 2>/dev/null
 cp /home/zditech/retrieved-backup/data/*.json /data/ 2>/dev/null
-if [ -d "/home/zditech/retrieved-backup/data/slides" ]; then
+if [ -d \"/home/zditech/retrieved-backup/data/slides\" ]; then
   mkdir /data/slides 2>/dev/null
   cp /home/zditech/retrieved-backup/data/slides/*.png /data/slides/ 2>/dev/null
 fi
 rm -r /home/zditech/retrieved-backup
 rm /home/zditech/retrieved-backup.tar.gz
-echo " "
-echo "Restore Complete"
-echo " "
+echo \" \"
+echo \"Restore Complete\"
+echo \" \"" > /home/zditech/restoreBackup.sh
