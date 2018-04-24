6b203b4df9259671057c8c8dc48bd0619bffd4fc
againeszdi <38566873+againeszdi@users.noreply.github.com>
2018-04-24 10:15:06 -0500

    Update backupTest.sh

--- backupTest.sh
+++ backupTest.sh
@@ -8,13 +8,10 @@ echo \"-----------------------------------------\"
 echo \" \"
 #send file to spark room
 postData=\$(curl -s -X POST -H \"Authorization: Bearer NmExYTNlYzQtYjc3Yy00MDdjLThiZmMtYzZjOTU3NDJjMzdjMjFhMmNiNTktOGQz\" -F \"files=@/home/zditech/\$filename.tar.gz;type=application/gzip\" -F \"roomId=Y2lzY29zcGFyazovL3VzL1JPT00vZWJmMWE0NTAtMTY1Ni0xMWU3LTg2YzgtMjMzN2U0Nzg0OTlm\" -F \"text=\$filename backup.\" https://api.ciscospark.com/v1/messages)
-backupData=\${postData::-1}
-backupData+=\",\"goName\":\"\$filename\"}\"
-echo \"\$backupData\"
+backupData=\${postData%?}
+backupData=\"\$backupData,\"goName\":\"\$filename\"}\"
 rm /home/zditech/\$filename.tar.gz
 #send file URL to spark room
 curl -s -X POST http://recvmsg.cwtest.ultrahook.com -d \"\$backupData\"
-echo \"Backup Complete\"
-echo \"-----------------------------------------\"
-echo \"COPY THE 'files' URL FROM THE ABOVE OUTPUT AND USE IT TO RUN THE restoreBackup.sh SCRIPT ONCE THE GO HAS BEEN MOVED.\"
-echo \"Example: sh restoreBackup.sh https://api.ciscospark.com/v1/contents/Y2lzY29zcGFyazovL3VzL0NPTlRFTlQvNzVlNDRjOC0xMWU4LWE5MTYtNzc3OGQ4ODNkNTBiLzA\"" > /home/zditech/dataBackup.sh
+echo \"Backup of \$filename complete\"
+echo \"-----------------------------------------\"" > /home/zditech/dataBackup.sh
